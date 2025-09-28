8b8880abfe710a86313b2c5606a4f8fac170d00c
Sivan <sivanjaadhav@gmail.com>
2025-09-28 12:06:41 +0530

    Update name in copyright notices

--- include/advin.h
+++ include/advin.h
@@ -1,5 +1,5 @@
 /*
- * © 2025 Sivan. All rights reserved.
+ * © 2025 Sivan Kiran Jadhav. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to use,
